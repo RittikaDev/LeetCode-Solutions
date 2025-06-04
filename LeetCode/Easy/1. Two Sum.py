fdfa6ed90566e84b0b2c025cad61f59b7d26d93d
Rittika Dev <rittika.dev@gmail.com>
2025-06-04 13:38:11 +0600

    first commit

---
+++ LeetCode/Easy/1. Two Sum.py
@@ -0,0 +1,22 @@
+﻿# 1. Two_sums
+from typing import List
+
+
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        num_dict = {}
+
+        for i, num in enumerate(nums):
+            complement = target - num
+
+            if complement in num_dict:
+                return [num_dict[complement], i]
+
+            num_dict[num] = i
+
+        return []
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    print(sol.twoSum([2, 7, 11, 15], 9)) 
