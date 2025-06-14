427e1058ccfd79bec5ee1e69af220cdf673b8c45
Rittika Dev <rittika.dev@gmail.com>
2025-06-14 15:44:01 +0600

    feat(python, easy): add solution for LeetCode #26 - Remove Duplicates from Sorted Array

--- LeetCode/Easy/1. Two Sum.py
+++ LeetCode/Easy/1. Two Sum.py
@@ -19,4 +19,4 @@ class Solution:
 
 if __name__ == "__main__":
     sol = Solution()
-    print(sol.twoSum([2, 7, 11, 15], 9)) 
+    print(sol.twoSum([2, 7, 11, 15], 9))
---
+++ LeetCode/Easy/26. Remove Duplicates from Sorted Array.py
@@ -0,0 +1,24 @@
+﻿from typing import List
+
+
+class Solution:
+    def removeDuplicates(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+
+        length = 1
+        for i in range(1, len(nums)):
+            if nums[i] != nums[length - 1]:
+                nums[length] = nums[i]
+                length += 1
+
+        return length
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1, 1, 2]
+    length = sol.removeDuplicates(nums)
+    print("Length:", length)
+    print("Modified nums:", nums[:length])
