d8817c8823c2c2cba074285fa4d271cb946435cd
Rittika Dev <rittika.dev@gmail.com>
2025-06-22 10:53:46 +0600

    feat(python, easy): add solution for LeetCode #118, #121

---
+++ LeetCode/Easy/118. Pascal's Triangle.py
@@ -0,0 +1,28 @@
+﻿from typing import List
+
+class Solution:
+    def generate(self, numRows: int) -> List[List[int]]:
+        arr = []
+
+        for row in range(numRows):
+            temp = []
+
+            for col in range(row + 1):
+                if col == 0 or col == row:
+                    temp.append(1)
+                else:
+                    val = arr[-1][col - 1] + arr[-1][col]
+                    temp.append(val)
+
+            arr.append(temp)
+
+        return arr
+
+
+
+# EXAMPLE USAGE
+if __name__ == "__main__":
+    sol = Solution()
+    numRows = 5
+    result = sol.generate(numRows)
+    print(result)
---
+++ LeetCode/Easy/19. Pascal's Triangle II.py
@@ -0,0 +1,28 @@
+﻿from typing import List
+
+class Solution:
+    def getRow(self, rowIndex: int) -> List[List[int]]:
+        arr = []
+
+        for row in range(rowIndex+1):
+            temp = []
+
+            for col in range(row + 1):
+                if col == 0 or col == row:
+                    temp.append(1)
+                else:
+                    val = arr[-1][col - 1] + arr[-1][col]
+                    temp.append(val)
+
+            arr.append(temp)
+
+        return arr[rowIndex]
+
+
+
+# EXAMPLE USAGE
+if __name__ == "__main__":
+    sol = Solution()
+    rowIndex = 3
+    result = sol.getRow(rowIndex)
+    print(result)
