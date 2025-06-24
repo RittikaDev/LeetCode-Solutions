89d95cc7b305d32d0d0f1afb1a70e9e61dadaab9
Rittika Dev <rittika.dev@gmail.com>
2025-06-24 16:12:46 +0600

    feat(python, easy): add solution for LeetCode #268, #283, #303

---
+++ LeetCode/Easy/268. Missing Number.py
@@ -0,0 +1,24 @@
+﻿from typing import List
+
+
+class Solution(object):
+    def missingNumber(self, nums:List[int]) -> int :
+          sumVal = 0
+          rValue = 0
+
+          for i in range(0, len(nums)):
+              sumVal = sumVal + nums[i]
+              orgSum = len(nums) * (len(nums) + 1) // 2
+              if orgSum > sumVal:
+                 rValue = orgSum - sumVal
+              else:
+                  rValue = 0
+
+          return rValue
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [1, 2]
+    result = sol.missingNumber(nums)
+    print(result)
---
+++ LeetCode/Easy/283. Move Zeroes.py
@@ -0,0 +1,16 @@
+﻿from typing import List
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        lastNonZeroFoundAt = 0
+
+        for i in range(len(nums)):
+            if nums[i] != 0:
+                nums[lastNonZeroFoundAt], nums[i] = nums[i], nums[lastNonZeroFoundAt]
+                lastNonZeroFoundAt += 1
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    nums = [0, 1, 0, 3, 12]
+    sol.moveZeroes(nums)
