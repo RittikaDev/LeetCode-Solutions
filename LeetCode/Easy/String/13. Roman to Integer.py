7620478faa2ae812f05223b55718c5f7c09e9d0b
Rittika Dev <rittika.dev@gmail.com>
2025-06-25 11:46:21 +0600

    feat(python, easy): add solution for LeetCode #13, #14

---
+++ LeetCode/Easy/String/13. Roman to Integer.py
@@ -0,0 +1,37 @@
+﻿class Solution(object):
+    def romanToInt(self, s: str) -> int :
+        symbolVal = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000,
+        }
+
+
+        returnVal = 0
+        i = 0
+
+        while i < len(s):
+            if i + 1 < len(s) and symbolVal[s[i]] < symbolVal[s[i + 1]]:
+                returnVal += symbolVal[s[i + 1]] - symbolVal[s[i]]
+                i += 2
+            else:
+                returnVal += symbolVal[s[i]]
+                i += 1
+
+        return returnVal
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "MCMXCIV"
+    result = sol.romanToInt(s)
+    print(result)
+
+
+# I can be placed before V (5) and X (10) to make 4 and 9.
+# X can be placed before L (50) and C (100) to make 40 and 90.
+# C can be placed before D (500) and M (1000) to make 400 and 900.
---
+++ LeetCode/Easy/String/14. Longest Common Prefix.py
@@ -0,0 +1,26 @@
+﻿from typing import List
+
+
+class Solution(object):
+    def longestCommonPrefix(self, strs:List[str]) -> str:
+        prefix = strs[0]
+
+        for i in range(1, len(strs)):
+            current = strs[i]
+            match_len = 0
+            for j in range(min(len(prefix), len(current))):
+                if prefix[j] == current[j]:
+                    match_len += 1
+                else:
+                    break
+            prefix = prefix[:match_len]
+            if prefix == "":
+                break
+
+        return prefix
+
+if __name__ == "__main__":
+    sol = Solution()
+    strs = ["flower","flow","flight"]
+    result = sol.longestCommonPrefix(strs)
+    print(result)
