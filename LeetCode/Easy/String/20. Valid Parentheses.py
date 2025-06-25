5f324e16a94eab8338e3cc0cafc76e1a3ee546e2
Rittika Dev <rittika.dev@gmail.com>
2025-06-25 16:38:22 +0600

    feat(python, easy): add solution for LeetCode #20, #28

---
+++ LeetCode/Easy/String/20. Valid Parentheses.py
@@ -0,0 +1,30 @@
+﻿class Solution(object):
+    def isValid(self, s:str) -> bool :
+        stack = []
+        mapping = {')': '(', ']': '[', '}': '{'}
+        rType = False
+
+        if len(s) % 2 != 0:
+            return False
+        for i, char in enumerate(s):
+            if char in "([{" :
+                stack.append(char)
+                rType = False
+            elif len(stack) > 1 and i == len(s) - 1:
+                return False
+            elif len(stack) > 0 and stack.pop() == mapping[char]:
+                rType = True
+            else:
+                return False
+
+        return rType
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    s = "(()("
+    result = sol.isValid(s)
+    print(result)
+
+
+# BEATS 100% USERS IN LEETCODE
---
+++ LeetCode/Easy/String/28. Find the Index of the First Occurrence in a String.py
@@ -0,0 +1,24 @@
+﻿class Solution(object):
+    def strStr(self, haystack:str, needle:str) -> int:
+        if not needle:
+            return 0
+
+        len_hay, len_ndl = len(haystack), len(needle)
+
+        for i in range(len_hay - len_ndl + 1):
+            if haystack[i:i + len_ndl] == needle:
+                return i
+
+        return -1
+
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    haystack = "leetcode"
+    needle = "leeto"
+    result = sol.strStr(haystack, needle)
+    print(result)
+
+
+# BEATS 100% USERS IN LEETCODE
