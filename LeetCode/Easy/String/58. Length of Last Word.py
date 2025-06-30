1a34ea7aee6316eb26b1f26b62daa40bdf7dca1c
Rittika Dev <rittika.dev@gmail.com>
2025-06-30 10:51:06 +0600

    feat(python, easy): add solution for LeetCode #58, #67

---
+++ LeetCode/Easy/String/58. Length of Last Word.py
@@ -0,0 +1,12 @@
+﻿class Solution(object):
+    def lengthOfLastWord(self, s:str) -> int:
+       return len(s.split()[-1])
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    sentence = "   fly me   to   the moon  "
+    result = sol.lengthOfLastWord(sentence)
+    print(result)
+
+# BEATS 100% USERS IN LEETCODE
---
+++ LeetCode/Easy/String/67. Add Binary.py
@@ -0,0 +1,15 @@
+﻿class Solution(object):
+    def addBinary(self, a:str, b:str) -> str :
+        return bin(int(a, 2) + int(b,2))[2:]
+
+
+if __name__ == "__main__":
+    sol = Solution()
+    a = "11"
+    b = "1"
+    result = sol.addBinary(a,b)
+    print(result)
+
+
+
+# BEATS 100% USERS IN LEETCODE
